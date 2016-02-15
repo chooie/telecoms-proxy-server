ff466d875a8a2bd78c8a5ea518bbb89d42b608b8
Charlie Hebert <charlie.hebert92@gmail.com>
2016-02-15 22:56:20 +0000

    Add placeholder lint task

--- build/scripts/Jakefile.js
+++ build/scripts/Jakefile.js
@@ -2,8 +2,13 @@
   "use strict";
 
   desc("Default Task");
-  task("default", function() {
+  task("default", [ "lint" ], function() {
     console.log("\n\nBUILD OK");
+  });
+
+  desc("Lint the Javascript code");
+  task("lint", function() {
+    console.log("Linting Javascripts: ");
   });
 
 }());
