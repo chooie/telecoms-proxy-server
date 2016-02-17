021fea8039180f95ecfa4a47c39db86b4ff29abe
Charlie Hebert <charlie.hebert92@gmail.com>
2016-02-17 11:04:38 +0000

    Use chai as assertion library

--- src/_server_test.js
+++ src/_server_test.js
@@ -1,9 +1,11 @@
 (function() {
   "use strict";
 
+  var assert = require("./assert.js");
+
   describe("Test", function() {
     it("passes", function() {
-      return;
+      assert.equal(true, true, "This should pass");
     });
   });
 
---
+++ src/assert.js
@@ -0,0 +1,8 @@
+(function() {
+  "use strict";
+
+  var assert = require("chai").assert;
+
+  module.exports = assert;
+
+}());
