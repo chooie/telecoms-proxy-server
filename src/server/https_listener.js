b5c2ccef0b65603a2d6e91c0e6229b05caded68a
Charlie Hebert <charlie.hebert92@gmail.com>
2016-02-22 22:35:40 +0000

    Support https

--- src/server/https_listener.js
+++ src/server/https_listener.js
@@ -1,119 +1,103 @@
 (function() {
   "use strict";
 
+  var debug = false;
+
   var net = require("net");
 
   module.exports = function(server) {
-    // add handler for HTTPS (which issues a CONNECT to the proxy)
+    // Add handler for HTTPS (which issues a CONNECT to the proxy)
     server.addListener('connect', httpsConnectHandler);
   };
 
   function httpsConnectHandler(request, socketRequest, bodyhead) {
-    console.log("HTTPS Request!!!");
+    if (debug) {
+      console.log("HTTPS Request!!!");
+    }
+
+    var url = request.url;
+    var httpVersion = request.httpVersion;
+    var hostport = getHostPortFromString(url, 443);
+
+    if (debug) {
+      console.log("Will connect to " + hostport[0] + ":" + hostport[1]);
+    }
+    // Set up TCP connection
+    var proxySocket = new net.Socket();
+    proxySocket.connect(parseInt(hostport[1]), hostport[0], function() {
+      if (debug) {
+        console.log("  < connected to %s/%s", hostport[0], hostport[1]);
+        console.log("  > writing head of length %d", bodyhead.length);
+      }
+
+      proxySocket.write(bodyhead);
+
+      // Tell the caller the connection was successfully established
+      socketRequest.write("HTTP/" + httpVersion + " 200 Connection " +
+        "established\r\n\r\n");
+    });
+
+    proxySocket.on("data", function(chunk) {
+      if (debug) {
+        console.log("  < data length = %d", chunk.length);
+      }
+      socketRequest.write( chunk );
+    });
+
+    proxySocket.on("end", function() {
+      if (debug) {
+        console.log("  < end");
+      }
+      socketRequest.end();
+    });
+
+    socketRequest.on("data", function(chunk) {
+      if (debug) {
+        console.log("  > data length = %d", chunk.length);
+      }
+      proxySocket.write(chunk);
+    });
+
+    socketRequest.on("end", function() {
+      if (debug) {
+        console.log( '  > end' );
+      }
+      proxySocket.end();
+    });
+
+    proxySocket.on("error", function(err) {
+      socketRequest.write("HTTP/" + httpVersion + " 500 Connection error" +
+        "\r\n\r\n");
+
+      if (debug) {
+        console.log("  < ERR: %s", err);
+      }
+      socketRequest.end();
+    });
+
+    socketRequest.on("error", function ( err ) {
+      if (debug) {
+        console.log( '  > ERR: %s', err );
+      }
+      proxySocket.end();
+    });
   }
 
-  //function httpsConnectHandler(request, socketRequest, bodyhead) {
-  //  var url = request['url'];
-  //  var httpVersion = request['httpVersion'];
-  //
-  //  var hostport = getHostPortFromString( url, 443 );
-  //
-  //  if ( debugging )
-  //    console.log( '  = will connect to %s:%s', hostport[0], hostport[1] );
-  //
-  //  // set up TCP connection
-  //  var proxySocket = new net.Socket();
-  //  proxySocket.connect(
-  //    parseInt( hostport[1] ), hostport[0],
-  //    function () {
-  //      if ( debugging )
-  //        console.log( '  < connected to %s/%s', hostport[0], hostport[1] );
-  //
-  //      if ( debugging )
-  //        console.log( '  > writing head of length %d', bodyhead.length );
-  //
-  //      proxySocket.write( bodyhead );
-  //
-  //      // tell the caller the connection was successfully established
-  //      socketRequest.write( "HTTP/" + httpVersion + " 200 Connection established\r\n\r\n" );
-  //    }
-  //  );
-  //
-  //  proxySocket.on(
-  //    'data',
-  //    function ( chunk ) {
-  //      if ( debugging )
-  //        console.log( '  < data length = %d', chunk.length );
-  //
-  //      socketRequest.write( chunk );
-  //    }
-  //  );
-  //
-  //  proxySocket.on(
-  //    'end',
-  //    function () {
-  //      if ( debugging )
-  //        console.log( '  < end' );
-  //
-  //      socketRequest.end();
-  //    }
-  //  );
-  //
-  //  socketRequest.on(
-  //    'data',
-  //    function ( chunk ) {
-  //      if ( debugging )
-  //        console.log( '  > data length = %d', chunk.length );
-  //
-  //      proxySocket.write( chunk );
-  //    }
-  //  );
-  //
-  //  socketRequest.on(
-  //    'end',
-  //    function () {
-  //      if ( debugging )
-  //        console.log( '  > end' );
-  //
-  //      proxySocket.end();
-  //    }
-  //  );
-  //
-  //  proxySocket.on(
-  //    'error',
-  //    function ( err ) {
-  //      socketRequest.write( "HTTP/" + httpVersion + " 500 Connection error\r\n\r\n" );
-  //      if ( debugging ) {
-  //        console.log( '  < ERR: %s', err );
-  //      }
-  //      socketRequest.end();
-  //    }
-  //  );
-  //
-  //  socketRequest.on(
-  //    'error',
-  //    function ( err ) {
-  //      if ( debugging ) {
-  //        console.log( '  > ERR: %s', err );
-  //      }
-  //      proxySocket.end();
-  //    }
-  //  );
-  //}
-  //
-  //function getHostPortFromString( hostString, defaultPort ) {
-  //  var host = hostString;
-  //  var port = defaultPort;
-  //
-  //  var result = regex_hostport.exec( hostString );
-  //  if ( result != null ) {
-  //    host = result[1];
-  //    if ( result[2] != null ) {
-  //      port = result[3];
-  //    }
-  //  }
-  //
-  //  return( [ host, port ] );
-  //}
+  function getHostPortFromString(hostString, defaultPort) {
+    var regex_hostport = /^([^:]+)(:([0-9]+))?$/;
+
+    var host = hostString;
+    var port = defaultPort;
+
+    var result = regex_hostport.exec(hostString);
+    if (result) {
+      host = result[1];
+      if (result[2]) {
+        port = result[3];
+      }
+    }
+
+    return([ host, port ]);
+  }
 
 }());
