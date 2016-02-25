548338feefbd6a74b868a38e4f48055546b2537a
Charlie Hebert <charlie.hebert92@gmail.com>
2016-02-25 13:54:18 +0000

    Tidy up server

--- src/server/server.js
+++ src/server/server.js
@@ -17,38 +17,11 @@
 
   function start(options, callback) {
     checkOptions(options);
-
-    var port = options.port;
-    var homePageToServe = options.homePageToServe;
-    var notFoundPageToServe = options.notFoundPageToServe;
-
     server = http.createServer();
-    server.on("request", function(clientRequest, responseToClient) {
-      route.modifyRequestIfFromLocalhost(clientRequest);
-
-      log("HTTP Request: " + clientRequest.url);
-
-      var url = clientRequest.url;
-
-      if (route.isHomeRoute(url)) {
-        response.homePageResponse(responseToClient, homePageToServe);
-
-      } else if (route.isBlockedInfoRoute(url)) {
-
-        if (clientRequest.method === "GET") {
-          urlBlocker.respondWithBlockedURLsJSON(responseToClient);
-        }
-
-      } else if (urlBlocker.isBlockedURL(url)) {
-        response.blockPageResponse(clientRequest, responseToClient);
-
-      } else {
-        request.resolveURL(clientRequest, responseToClient, notFoundPageToServe,
-          request.proxyRequest);
-      }
-    });
+    var curryResponseWithOptions = respondToRequest.bind(null, options);
+    server.on("request", curryResponseWithOptions);
     httpsListener(server);
-    server.listen(port, callback);
+    server.listen(options.port, callback);
   }
 
   function stop(callback) {
@@ -67,9 +40,57 @@
     }
   }
 
+  function respondToRequest(options, clientRequest, responseToClient) {
+    route.modifyRequestIfFromLocalhost(clientRequest);
+    log("HTTP Request: " + clientRequest.url);
+    respond(options.homePageToServe, options.notFoundPageToServe,
+      clientRequest, responseToClient);
+  }
+
+  function respond(homePageToServe, notFoundPageToServe, clientRequest,
+                   responseToClient) {
+    var url = clientRequest.url;
+
+    if (route.isHomeRoute(url)) {
+      serveHome(responseToClient, homePageToServe);
+    } else if (route.isBlockedInfoRoute(url)) {
+      serveBlockedInfo(clientRequest, responseToClient, notFoundPageToServe);
+    } else if (urlBlocker.isBlockedURL(url)) {
+      blockRequest(clientRequest, responseToClient);
+    } else {
+      requestThenServe(clientRequest, responseToClient, notFoundPageToServe);
+    }
+  }
+
+  function serveHome(responseToClient, homePageToServe) {
+    response.homePageResponse(responseToClient, homePageToServe);
+  }
+
+  function serveBlockedInfo(clientRequest, responseToClient,
+                            notFoundPageToServe) {
+    if (isGetRequest(clientRequest)) {
+      urlBlocker.respondWithBlockedURLsJSON(responseToClient);
+    } else {
+      response.notFoundPageResponse(responseToClient, notFoundPageToServe);
+    }
+  }
+
+  function blockRequest(clientRequest, responseToClient) {
+    response.blockPageResponse(clientRequest, responseToClient);
+  }
+
+  function requestThenServe(clientRequest, responseToClient,
+                                         notFoundPageToServe) {
+    request.resolveURL(clientRequest, responseToClient, notFoundPageToServe,
+      request.proxyRequest);
+  }
+
+  function isGetRequest(clientRequest) {
+    return clientRequest.method === "GET";
+  }
+
   module.exports = {
     start: start,
     stop: stop,
   };
-
 }());
